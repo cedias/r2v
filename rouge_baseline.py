2398322f9001a884914d1a76243db4b56b27b58a
charles-emmanuel dias <charles-emmanuel.dias@etu.upmc.fr>
2015-06-09 18:40:02 +0200

    Rouge baselining possible

---
+++ rouge_baseline.py
@@ -0,0 +1,147 @@
+import sqlite3
+import argparse
+from gensim.models.doc2vec import Doc2Vec
+import numpy as np
+from gensim import matutils
+from random import shuffle,choice
+
+def getItemReviews(item, db):
+    con = sqlite3.connect(db)
+    c = con.cursor()
+    c.execute("SELECT user,rating,review FROM reviews WHERE item = {} and not test".format(item))
+    return c.fetchall()
+
+def getUserReviews(user, db):
+    con = sqlite3.connect(db)
+    c = con.cursor()
+    c.execute("SELECT item,rating,review FROM reviews WHERE user = {} and not test".format(user))
+    return c.fetchall()
+
+def getAllReviews(db,test=False):
+    con = sqlite3.connect(db)
+    c = con.cursor()
+    if test:
+        c.execute("SELECT item,user,rating FROM reviews WHERE test ORDER BY user")
+    else:
+        c.execute("SELECT item,user,rating FROM reviews WHERE not test ORDER BY user")
+    return c.fetchall()
+
+def getReviewText(db,user,item):
+    con = sqlite3.connect(db)
+    c = con.cursor()
+    c.execute("SELECT review FROM reviews WHERE user = {} and item = {}".format(user,item))
+    return c.fetchone()[0]
+
+def getUsersBias(db):
+    con = sqlite3.connect(db)
+    c = con.cursor()
+    c.execute("SELECT user,avg(rating) as bias FROM reviews WHERE not test group by user")
+    return c.fetchall()
+
+def getItemsBias(db):
+    con = sqlite3.connect(db)
+    c = con.cursor()
+    c.execute("SELECT item, avg(rating) as bias FROM reviews WHERE not test group by item")
+    return c.fetchall()
+
+
+
+def find_ngrams(input_list, n):
+  return {" ".join(p) for p in zip(*[input_list[i:] for i in range(n)])}
+
+
+def rouge_1_2_3_metric(words_real,words_pred):
+    words_real_n1 = find_ngrams(words_real,1)
+    words_pred_n1 = find_ngrams(words_pred,1)
+
+    words_real_n2 = find_ngrams(words_real,2)
+    words_pred_n2 = find_ngrams(words_pred,2)
+
+    words_real_n3 = find_ngrams(words_real,3)
+    words_pred_n3 = find_ngrams(words_pred,3)
+
+    r1 = len(words_real_n1.intersection(words_pred_n1))/(len(words_real_n1)+0.0)
+    r2 = len(words_real_n2.intersection(words_pred_n2))/(len(words_real_n2)+0.0)
+    r3 = len(words_real_n3.intersection(words_pred_n3))/(len(words_real_n3)+0.0)
+
+    return (r1,r2,r3)
+
+
+def k_sim(model, db,neigh="user"):
+
+    if neigh not in {"user","item"}:
+        print("only {} as similarity".format(["user","item"]))
+
+    print("prepping data")
+    test_data = [(item, user, rating) for item, user, rating in getAllReviews(db, test=True)]
+    shuffle(test_data)
+    print("test data ready")
+
+
+    cpt_test = 0
+    cpt_skipped = 0
+
+    oracle_r1 = 0
+    oracle_r2 = 0
+    oracle_r3 = 0
+
+    random_r1 = 0
+    random_r2 = 0
+    random_r3 = 0
+
+    for item, user, _ in test_data:
+
+        if cpt_test >= len(test_data)/2: # we only evaluate on random 50%
+            break
+
+        if "u_{}".format(user) not in model.vocab or "i_{}".format(item) not in model.vocab: #skip not in vocab
+            cpt_skipped += 1
+            continue
+
+
+        if neigh == "user":
+            list_text = [stext.replace("."," ").lower().split(" ") for suser,_,stext in getItemReviews(item, db) if "u_{}".format(suser) in model.vocab]
+
+        elif neigh == "item":
+            list_text = [stext.replace("."," ").lower().split(" ") for sitem,_,stext in getUserReviews(user, db) if "i_{}".format(sitem) in model.vocab]
+        else:
+            raise Exception("Neigh not item nor user")
+
+        if len(list_text) == 0:
+            cpt_skipped +=1
+            continue
+
+
+        rtext = getReviewText(db,user,item)
+        rtext = rtext.replace("."," ").lower().split(" ")
+
+        rouges = [rouge_1_2_3_metric(rtext,t) for t in list_text ]
+
+        rand_choice = choice(rouges)
+        best_choice = np.max(rouges,axis=0)
+
+        oracle_r1 += best_choice[0]
+        oracle_r2 += best_choice[1]
+        oracle_r3 += best_choice[2]
+
+        random_r1 += rand_choice[0]
+        random_r2 += rand_choice[1]
+        random_r3 += rand_choice[2]
+
+        cpt_test += 1
+        if cpt_test % 100 == 0:
+            print("R1:[{},{}], R2:[{},{}], R3:[{},{}]".format( random_r1/(cpt_test+0.0),oracle_r1/(cpt_test+0.0),random_r2/(cpt_test+0.0),oracle_r2/(cpt_test+0.0),random_r3/(cpt_test+0.0),oracle_r3/(cpt_test+0.0)))
+
+    print("R1:[{},{}], R2:[{},{}], R3:[{},{}]".format( random_r1/(cpt_test+0.0),oracle_r1/(cpt_test+0.0),random_r2/(cpt_test+0.0),oracle_r2/(cpt_test+0.0),random_r3/(cpt_test+0.0),oracle_r3/(cpt_test+0.0)))
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--neigh",default="item", type=str)
+parser.add_argument("model", type=str)
+parser.add_argument("db", type=str)
+
+args = parser.parse_args()
+db = args.db
+model = Doc2Vec.load_word2vec_format(args.model, binary=True,norm_only=False)
+k_sim(model,db,neigh=args.neigh)
