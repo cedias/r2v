1b7d669f72bacbf0e3d74ee20d8d1fbfe33942a9
charles-emmanuel <charles-emmanuel.dias@etu.upmc.fr>
2015-11-06 18:42:58 +0100

    euclidean and subsampling

--- d2v/w2p.c
+++ d2v/w2p.c
@@ -482,7 +482,7 @@ void *TrainModelThread(void *id) {
                     break;
 
                 // The subsampling randomly discards frequent words while keeping the ranking same
-                if (sample > 0) {
+                if (sample > 0 && sentence_length > 2) {
                     real ran = (sqrt(vocab[word].cn / (sample * train_words)) + 1) * (sample * train_words) / vocab[word].cn;
                     next_random = next_random * (unsigned long long)25214903917 + 11;
 
--- predict_near.py
+++ predict_near.py
@@ -18,29 +18,29 @@ def getAllReviews(db, test=False):
 
 
 def k_sim(model, db):
-
     print("prepping data")
+    test_data = [(model["u_{}".format(user)] + model["i_{}".format(item)] , float(rating)) for item, user, rating in getAllReviews(db, test=True) if "u_{}".format(user) in model.vocab and "i_{}".format(item) in model.vocab]
+    
+    #test_data = test_data[0:5000]
 
-    test_data = [(matutils.unitvec(model["u_{}".format(user)] + model["i_{}".format(item)]) , float(rating)) for item, user, rating in getAllReviews(db, test=True) if "u_{}".format(user) in model.vocab and "i_{}".format(item) in model.vocab]
-    rating_indexs = [(matutils.unitvec(model[word]), float(word.split("_")[1])) for word in model.index2word if len(word) > 2 and word[0] == "r" and word[1] == "_"]
-
+    rating_indexs = [(model[word], float(word.split("_")[1])) for word in model.index2word if len(word) > 2 and word[0] == "r" and word[1] == "_"]
     r_vec, ratings = zip(*rating_indexs)
     test_vec, ground_truth = zip(*test_data)
-    rand = [randint(0,len(ratings)-1)for i in range(0,len(test_data))]
+    rand = [randint(0, len(ratings) - 1)for i in range(0, len(test_data))]
     print("{} test data ready".format(len(test_data)))
+
     ratings = np.array(ratings)
     ground_truth = np.array(ground_truth)
-    r_vec = np.array(r_vec).T
-    test_vec = np.array(test_vec)
-    
+    index = np.zeros(len(test_vec),dtype=int)
+    min_d = np.ones(len(test_vec)) * 10000000000
 
+    for i, r_i in enumerate(r_vec):
+        norm = np.linalg.norm(test_vec - r_i, 2, axis=1)  # np.sqrt(np.sum((test_vec - r_i) ** 2,axis=1))
+        where = np.where(norm < min_d)
+        min_d[where] = norm
+        index[where] = i
 
-    dp = np.dot(test_vec,r_vec)
-    print(dp.shape)
-    err = (ratings[np.argmax(dp, axis=1)] - ground_truth) ** 2
-    print(np.mean(err))
-
-    err = (ratings[rand] - ground_truth) ** 2
+    err = (ratings[index] - ground_truth) ** 2
     print(np.mean(err))
 
 
@@ -51,5 +51,5 @@ parser.add_argument("db", type=str)
 
 args = parser.parse_args()
 db = args.db
-model = Doc2Vec.load_word2vec_format(args.model, binary=True,norm_only=False)
+model = Doc2Vec.load_word2vec_format(args.model, binary=True, norm_only=False)
 k_sim(model, db)
