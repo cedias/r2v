7378c43c2677e18ad0983fdd4bd35a17b8374cc8
Charles-Emmanuel Dias <Charles-Emmanuel.Dias@etu.upmc.fr>
2015-12-08 13:27:19 +0100

    new version of predict_rating

--- predict_rating.py
+++ predict_rating.py
@@ -4,6 +4,8 @@ from gensim.models.doc2vec import Doc2Vec
 import numpy as np
 from gensim import matutils
 from random import shuffle
+import pickle
+from sklearn.preprocessing import normalize
 
 def getItemReviews(item, db):
     con = sqlite3.connect(db)
@@ -58,7 +60,7 @@ def k_sim(model, db,k=None,neigh="user",mean_norm=False):
 
     cpt_test = 0
     cpt_skipped = 0
-    err = 0
+    tot_err = np.zeros(k)
 
     for item, user, rating in test_data:
 
@@ -82,19 +84,19 @@ def k_sim(model, db,k=None,neigh="user",mean_norm=False):
 
         if neigh == "user":
             if mean_norm:
-                list_sims = [(suser,srating-u_bias[suser],np.dot(matutils.unitvec(model["u_{}".format(suser)]),vect)) for suser,srating,_ in getItemReviews(item, db) if "u_{}".format(suser) in model.vocab]
+                list_sims = [(suser,srating-u_bias[suser],model["u_{}".format(suser)]) for suser,srating,_ in getItemReviews(item, db) if "u_{}".format(suser) in model.vocab]
             else:
-                list_sims = [(suser,srating,np.dot(matutils.unitvec(model["u_{}".format(suser)]),vect)) for suser,srating,_ in getItemReviews(item, db) if "u_{}".format(suser) in model.vocab]
+                list_sims = [(suser,srating,model["u_{}".format(suser)]) for suser,srating,_ in getItemReviews(item, db) if "u_{}".format(suser) in model.vocab]
 
         elif neigh == "item":
             if mean_norm:
-                list_sims = [(sitem,srating-i_bias[sitem],np.dot(matutils.unitvec(model["i_{}".format(sitem)]),vect)) for sitem,srating,_ in getUserReviews(user, db) if "i_{}".format(sitem) in model.vocab]
+                list_sims = [(sitem,srating-i_bias[sitem],model["i_{}".format(sitem)]) for sitem,srating,_ in getUserReviews(user, db) if "i_{}".format(sitem) in model.vocab]
             else:
-                list_sims = [(sitem,srating,np.dot(matutils.unitvec(model["i_{}".format(sitem)]),vect)) for sitem,srating,_ in getUserReviews(user, db) if "i_{}".format(sitem) in model.vocab]
+                list_sims = [(sitem,srating,model["i_{}".format(sitem)]) for sitem,srating,_ in getUserReviews(user, db) if "i_{}".format(sitem) in model.vocab]
 
         elif neigh == "sum":
-                list_sims = [(suser,srating,np.dot(matutils.unitvec(model["u_{}".format(suser)]+model["i_{}".format(item)]),vect)) for suser,srating,_ in getItemReviews(item, db) if "u_{}".format(suser) in model.vocab]
-                list_sims += [(sitem,srating,np.dot(matutils.unitvec(model["i_{}".format(sitem)]+model["u_{}".format(user)]),vect)) for sitem,srating,_ in getUserReviews(user, db) if "i_{}".format(sitem) in model.vocab]
+                list_sims = [(suser,srating,model["u_{}".format(suser)]+model["i_{}".format(item)]) for suser,srating,_ in getItemReviews(item, db) if "u_{}".format(suser) in model.vocab]
+                list_sims += [(sitem,srating,model["i_{}".format(sitem)]+model["u_{}".format(user)]) for sitem,srating,_ in getUserReviews(user, db) if "i_{}".format(sitem) in model.vocab]
 
 
         if len(list_sims) == 0:
@@ -102,24 +104,37 @@ def k_sim(model, db,k=None,neigh="user",mean_norm=False):
             continue
 
         sim_users,sim_rating,sim_sim = zip(*list_sims)
+        sim_rating = np.array(sim_rating)
+        sim_sim = np.array(sim_sim)
 
-        sim_sim = [(x+1)/(2+0.0) for x in sim_sim] # make sim between [0,1]
+        sim_sim = normalize(sim_sim, copy=False)
+        sim_sim = np.dot(vect,sim_sim.T)
+
+        sim_sim += 1
+        sim_sim /= 2.0
         order = np.argsort(sim_sim)[::-1]
 
         if len(order) == 0:
             cpt_skipped +=1
             continue
         
-        if k is not None:    
-            order = order[:k]
+      
 
+        order = np.array(order)
+        sim_sim = sim_sim[order]
+        sim_rating = sim_rating[order]
+
+        sim_sim = sim_sim[:k]
+        sim_rating = sim_rating[:k]
         
-        sum_rs = sum([sim_rating[x]*sim_sim[x] for x in order])
-        sum_sim = sum([sim_sim[x] for x in order])
+        pond = sim_rating * sim_sim
 
-
+        sum_rs = np.cumsum(pond)
+        sum_sim = np.cumsum(sim_sim)
+       
 
         predicted = sum_rs/(sum_sim+0.0)
+
 
         if mean_norm:
             if neigh == "user":
@@ -128,13 +143,25 @@ def k_sim(model, db,k=None,neigh="user",mean_norm=False):
                 predicted +=  i_bias[item]
 
 
-        err += (rating - predicted) ** 2
+
+        err = (rating - predicted) ** 2
+
+        if len(err != k):
+            oldlen = len(err)
+            err.resize(k,refcheck=False)
+            err[oldlen:k] = err[oldlen-1]
+
+        tot_err += err
+
+        
 
         cpt_test += 1
+        
         if cpt_test % 100 == 0:
-            print("MSE at {} tests is {} - {} test cases where skipped".format(cpt_test,err/(cpt_test+0.0),cpt_skipped))
+            print("MSE at {} tests is {} - {} test cases where skipped".format(cpt_test,tot_err/(cpt_test+0.0),cpt_skipped))
 
-    print("Final MSE for {} tests is {} - {} test cases where skipped".format(cpt_test,err/(cpt_test+0.0),cpt_skipped))
+    print("Final MSE for {} tests is {} - {} test cases where skipped".format(cpt_test,tot_err/(cpt_test+0.0),cpt_skipped))
+    return tot_err/(cpt_test+0.0)
 
 
 parser = argparse.ArgumentParser()
@@ -142,10 +169,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--k",default=5, type=int)
 parser.add_argument("--neigh",default="item", type=str)
 parser.add_argument('--mean_center', dest='mean_center', action='store_true')
+parser.add_argument('--output', default="predicted_ratings.pkl",type=str)
+
 parser.add_argument("model", type=str)
 parser.add_argument("db", type=str)
+
 
 args = parser.parse_args()
 db = args.db
 model = Doc2Vec.load_word2vec_format(args.model, binary=True,norm_only=False)
-k_sim(model,db,k=args.k,neigh=args.neigh,mean_norm=args.mean_center)
+err = k_sim(model,db,k=args.k,neigh=args.neigh,mean_norm=args.mean_center)
+
+result = {
+    "neigh":args.neigh,
+    "mean_center":args.mean_center,
+    "model":args.model,
+    "db":args.db,
+    "error":err
+}
+
+pickle.dump(result,open(args.output,"wb"))
